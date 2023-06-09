8ebab0dda70c10eeb61b0d7d6ab1cc990f072d46
QuackQuack <suddorun@gmail.com>
2023-06-09 23:57:44 +0700

    add some code

--- src/crawler/entovi.rs
+++ src/crawler/entovi.rs
@@ -4,9 +4,9 @@ use anyhow::bail;
 use async_trait::async_trait;
 use tl::{ParserOptions, VDom};
 
-use crate::helper::string_helper::join_split;
+use crate::{helper::string_helper::join_split, parser::Parser};
 
-use super::{WordCrawler, WordDefinition};
+use super::{WordCrawler, WordDefinition, WordTypeDefinition};
 
 pub struct EnToViCrawler {
     client: reqwest::Client,
@@ -36,77 +36,63 @@ impl EnToViCrawler {
         )
     }
 
-    fn parse_html_content(&self, html_content: &str) -> anyhow::Result<WordDefinition> {
-        let dom = tl::parse(html_content, ParserOptions::default())?;
-        let word = self.extract_word(&dom);
-        if word.is_none() {
-            bail!("Doesn't contain a word")
-        }
-        let pronounce = self.query_selector_html_inner(&dom);
+    // fn extract_word(&self, dom: &VDom) -> Option<String> {
+    //     let parser = dom.parser();
+    //     let query_selector_iter = dom.query_selector("div.w.fl");
 
-        Ok(WordDefinition::new(
-            &word.unwrap(),
-            &pronounce.unwrap_or("".to_string()),
-        ))
-    }
+    //     if query_selector_iter.is_none() {
+    //         return None;
+    //     }
 
-    fn extract_word(&self, dom: &VDom) -> Option<String> {
-        let parser = dom.parser();
-        let query_selector_iter = dom.query_selector("div.w.fl");
+    //     let elements = query_selector_iter.unwrap();
 
-        if query_selector_iter.is_none() {
-            return None;
-        }
+    //     for el in elements {
+    //         let node = el.get(parser);
+    //         if node.is_none() {
+    //             continue;
+    //         }
 
-        let elements = query_selector_iter.unwrap();
+    //         let actual_node = node.unwrap();
 
-        for el in elements {
-            let node = el.get(parser);
-            if node.is_none() {
-                continue;
-            }
+    //         match actual_node {
+    //             tl::Node::Tag(html) => {
+    //                 return Some(html.inner_text(&parser).to_string());
+    //             }
+    //             _ => continue,
+    //         }
+    //     }
 
-            let actual_node = node.unwrap();
+    //     None
+    // }
 
-            match actual_node {
-                tl::Node::Tag(html) => {
-                    return Some(html.inner_text(&parser).to_string());
-                }
-                _ => continue,
-            }
-        }
+    // fn query_selector_html_inner(&self, dom: &VDom) -> Option<String> {
+    //     let parser = dom.parser();
+    //     let query_selector_iter = dom.query_selector("div.p5l.fl.cB");
 
-        None
-    }
+    //     if query_selector_iter.is_none() {
+    //         return None;
+    //     }
 
-    fn query_selector_html_inner(&self, dom: &VDom) -> Option<String> {
-        let parser = dom.parser();
-        let query_selector_iter = dom.query_selector("div.p5l.fl.cB");
+    //     let elements = query_selector_iter.unwrap();
 
-        if query_selector_iter.is_none() {
-            return None;
-        }
+    //     for el in elements {
+    //         let node = el.get(parser);
+    //         if node.is_none() {
+    //             continue;
+    //         }
 
-        let elements = query_selector_iter.unwrap();
+    //         let actual_node = node.unwrap();
 
-        for el in elements {
-            let node = el.get(parser);
-            if node.is_none() {
-                continue;
-            }
+    //         match actual_node {
+    //             tl::Node::Tag(html) => {
+    //                 return Some(html.inner_text(&parser).to_string());
+    //             }
+    //             _ => continue,
+    //         }
+    //     }
 
-            let actual_node = node.unwrap();
-
-            match actual_node {
-                tl::Node::Tag(html) => {
-                    return Some(html.inner_text(&parser).to_string());
-                }
-                _ => continue,
-            }
-        }
-
-        None
-    }
+    //     None
+    // }
 
     async fn fetch_html(&self, endpoint: &'_ str) -> reqwest::Result<String> {
         let response = self
@@ -118,6 +104,27 @@ impl EnToViCrawler {
             .await?;
         Ok(response)
     }
+
+    fn get_definition(&self, parser: &Parser) -> Option<WordDefinition> {
+        let word = parser.query_selector_first_element_inner_text("div.w.fl");
+        if word.is_none() {
+            return None;
+        }
+
+        let pronounce = parser.query_selector_first_element_inner_text("div.p5l.fl.cB");
+
+        let html_tags = parser.query_selector_elements("[id^='partofspeech']");
+
+        return Some(WordDefinition::new(
+            &word.unwrap(),
+            &pronounce.unwrap_or("".to_string()),
+        ));
+    }
+
+    // fn get_word_type_and_example(parser: &Parser) -> Vec<WordTypeDefinition> {
+    //     let partofspeech_parts = parser.query_selector_elements("[id^='partofspeech']");
+    //     todo!()
+    // }
 }
 
 #[async_trait]
@@ -125,7 +132,11 @@ impl WordCrawler for EnToViCrawler {
     async fn crawl(&self, word: &str) -> anyhow::Result<super::WordDefinition> {
         let endpoint = self.construct_query_endpoint(word);
         let html = self.fetch_html(&endpoint).await?;
-        let definitions = self.parse_html_content(&html);
-        definitions
+        let parser = Parser::new(&html)?;
+        let definition = self.get_definition(&parser);
+        if definition.is_none() {
+            bail!("word not found");
+        }
+        Ok(definition.unwrap())
     }
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,18 +1,72 @@
-use crate::crawler::WordDefinition;
+use std::vec;
+
+use tl::{HTMLTag, ParserOptions, VDom};
+
+pub type InnerText = String;
 
 pub struct Parser<'a> {
-    inner_html: &'a str,
+    dom: VDom<'a>,
 }
 
 impl<'a> Parser<'a> {
-    pub fn new(html: &'a str) -> Self {
-        Self { inner_html: html }
+    pub fn new(html: &'a str) -> anyhow::Result<Self> {
+        let dom = tl::parse(html, ParserOptions::default())?;
+        Ok(Self { dom })
     }
 
-    pub fn query_selector<P>(selector: P) -> anyhow::Result<String>
+    pub fn query_selector_first_element_inner_text<P>(&self, selector: P) -> Option<InnerText>
     where
-        P: AsRef<&'a str>,
+        P: AsRef<str>,
     {
-        todo!()
+        let tags = self.query_selector_elements(selector);
+
+        if let Some(tag) = tags.first() {
+            return Some((*tag.inner_text(self.dom.parser())).to_string());
+        }
+
+        None
+    }
+
+    //get html tag from query selector, ignores all nodes even we use nodes api
+    pub fn query_selector_elements<P>(&self, selector: P) -> Vec<&HTMLTag>
+    where
+        P: AsRef<str>,
+    {
+        let mut result = vec![];
+        let parser = self.dom.parser();
+        let query_selector_iter = self.dom.query_selector(selector.as_ref());
+
+        if query_selector_iter.is_none() {
+            return result;
+        }
+        let node_handles = query_selector_iter.unwrap();
+
+        for node_handle in node_handles {
+            let actual_node = node_handle.get(parser);
+            if actual_node.is_none() {
+                continue;
+            }
+
+            //try to convert node -> html tag, if not a tag, just ignore it
+            let html_tag = actual_node.unwrap().as_tag();
+            if html_tag.is_none() {
+                continue;
+            }
+
+            result.push(html_tag.unwrap());
+        }
+
+        result
+    }
+
+    pub fn has_id(&self, tag: &HTMLTag, id: &str) -> bool {
+        let attributes = tag.attributes();
+        for attribute in attributes.iter() {
+            if attribute.0 == "id" && !attribute.1.is_none() && attribute.1.unwrap() == id {
+                return true;
+            }
+        }
+
+        false
     }
 }
